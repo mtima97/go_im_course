e040910b3747bfb7b21f0c9cb09398a6d945c679
Timur <mtima97@gmail.com>
2025-10-23 18:25:24 +0000

    second commit

--- cmd/main.go
+++ cmd/main.go
@@ -1,7 +1,16 @@
 package main
 
-import "fmt"
+import (
+	"fmt"
+	"log"
+	"os"
+)
 
 func main() {
-	fmt.Println("Hello World")
+	bs, err := os.ReadFile("/IdeaProjects/gocourse/data/test.txt")
+	if err != nil {
+		log.Fatal(err)
+	}
+
+	fmt.Println("Hello World", string(bs))
 }
