c7577040d9f0eb59d16c1f21164877a96256ede3
Timur <mtima97@gmail.com>
2025-10-27 10:25:50 +0000

    feat: add course samples

--- cmd/main.go
+++ cmd/main.go
@@ -2,15 +2,40 @@ package main
 
 import (
 	"fmt"
+	"gocourse/internal/service/files"
+	"io"
 	"log"
-	"os"
+	"net/http"
 )
 
+//goland:noinspection ALL
 func main() {
-	bs, err := os.ReadFile("/IdeaProjects/gocourse/data/test.txt")
-	if err != nil {
-		log.Fatal(err)
-	}
+	mux := http.NewServeMux()
 
-	fmt.Println("Hello World", string(bs))
+	mux.HandleFunc("/", func(w http.ResponseWriter, r *http.Request) {
+		text, err := files.Read()
+		if err != nil {
+			http.Error(w, err.Error(), http.StatusInternalServerError)
+			return
+		}
+
+		fmt.Fprintln(w, text)
+	})
+
+	mux.HandleFunc("POST /", func(w http.ResponseWriter, r *http.Request) {
+		body, err := io.ReadAll(r.Body)
+		if err != nil {
+			http.Error(w, err.Error(), http.StatusBadRequest)
+		}
+		defer r.Body.Close()
+
+		fmt.Fprintln(w, string(body))
+	})
+
+	// Make GET
+	// Make POST
+
+	if err := http.ListenAndServe(":7999", mux); err != nil {
+		log.Fatal("Error starting server:", err)
+	}
 }
